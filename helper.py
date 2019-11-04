94054984e76c8efb93bb7b898d19c5c303c0a577
JunQuann <limjunquann@gmail.com>
2019-11-04 12:11:26 +0800

    testing SVM

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import cv2
+from matplotlib import pyplot as plt
 
 
 def convert(frame, src_model="bgr", dest_model="hls"):
@@ -25,3 +26,36 @@ def convert(frame, src_model="bgr", dest_model="hls"):
         raise Exception("ERROR:", "src_model or dest_model not implemented")
 
     return frame
+
+
+def sliding_window(img, step_size, window_size):
+    height, width, channel = img.shape
+    window_width, window_height = window_size
+    for y in range(0, height, step_size):
+        for x in range(0, width, step_size):
+            frame = img[y : y + window_height, x : x + window_width]
+            frame_height, frame_width, channel = frame.shape
+            x_min, y_min = x, y
+
+            # if frame_height != window_height:
+            #     y_min = height - window_height
+            # if frame_width != window_width:
+            #     x_min = width - window_width
+            # frame = img[y_min : y + window_height, x_min : x_min + window_width]
+            if frame_height != window_height or frame_width != window_width:
+                continue
+
+            yield (x_min, y_min, frame)
+
+
+def show_images(imgs, per_row=3, per_col=2, W=10, H=5, tdpi=80):
+    fig, ax = plt.subplots(per_col, per_row, figsize=(W, H), dpi=tdpi)
+    ax = ax.ravel()
+
+    for i in range(len(imgs)):
+        img = imgs[i]
+        ax[i].imshow(img)
+
+    for i in range(per_row * per_col):
+        ax[i].axis("off")
+
